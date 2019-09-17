dd3e9cc4c977d435e497a4e260cce13bd3c29d99
Emily Stacy <Emily.Stacy@improving.com>
2019-09-17 16:08:18 -0500

    tag 201909171100

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -11,24 +11,31 @@ public class Main {
         //primitive type don't follow upper case convention like char, int (better with memory management for computers)
         //String and Scanner for example are NOT primitive type
         //input = input.trim() => set all input with a trimmed value
-        while(true) {
+        boolean loop = true;
+        while(loop) {
             System.out.print("> ");
             String input = scanner.nextLine().trim();
-         //conditional statement
-            if(input.equals("look")){
+            //conditional statement
+            if (input.equals("look")) {
                 System.out.println("You look around.");
-            }
-            if(input.equals("inventory")){
+            }else if (input.equals("inventory")) {
                 System.out.println("You are carrying nothing.");
-            }
-         // equalsIgnoreCase():https://www.w3resource.com/java-tutorial/string/string_equalsignorecase.php
-            if(input.equalsIgnoreCase("dance")){
+            }else if (input.equalsIgnoreCase("dance")) {
                 System.out.println("You dance around.");
-            }
-            if(input.equalsIgnoreCase("jump")){
+            } else if (input.equalsIgnoreCase("jump")) {
                 System.out.println("You jump in the air.");
+            }else if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Goodbye.");
+                loop =false;
+            }else {
+                System.out.println("Huh? I don't understand");
             }
+            // equalsIgnoreCase():https://www.w3resource.com/java-tutorial/string/string_equalsignorecase.php
 
+
+
+/*break won't run codes after the break line in the loop; return will directly run the last line of the code;boolean will run all the codes until the method ends
+ System.out.println("Command processed"); continue will jump back to the condition*/
         }
     }
 }
