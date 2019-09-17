1cb544535c1c8e87340d7a3054763e71aa2a7300
Emily Stacy <Emily.Stacy@improving.com>
2019-09-17 13:47:10 -0500

    trim strategy

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -4,6 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        //[type] [name]; => var declaration in Java
+        //[type] [name] = [value]; => assigned an initial value
+        //In Java class starts with a uppercase (convention)
+        //primitive type don't follow upper case convention like char, int (better with memory management for computers)
+        //String and Scanner for example are NOT primitive type
+        //input = input.trim() => set all input with a trimmed value
+        while(true) {
+            System.out.print("> ");
+            String input = scanner.nextLine().trim();
+         //conditional statement
+            if(input.equals("look")){
+                System.out.println("You look around.");
+            }
+            if(input.equals("inventory")){
+                System.out.println("You are carrying nothing.");
+            }
+         // equalsIgnoreCase():https://www.w3resource.com/java-tutorial/string/string_equalsignorecase.php
+            if(input.equalsIgnoreCase("dance")){
+                System.out.println("You dance around.");
+            }
+            if(input.equalsIgnoreCase("jump")){
+                System.out.println("You jump in the air.");
+            }
+
+        }
     }
 }
