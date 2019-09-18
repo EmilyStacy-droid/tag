1abe57a332f7ca2042f1f69701ee4763675a06e0
Emily Stacy <Emily.Stacy@improving.com>
2019-09-18 15:51:40 -0500

    adding game.run

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -1,44 +1,27 @@
 package org.improving.tag;
 
 import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    /* example of polymorphism
+/* example of polymorphism
       Object bar = new Game(); => cannot access to the methods of Game because bar = Object
       System.out.println((Game).bar.getStartTime()); => just in this moment make bar = Game
     */
 
-        Game game = new Game();
-        System.out.println(game.getStartTime());
-        scanner.nextLine();
-        Game g2  = new Game();
-        System.out.println(g2.getStartTime());
-        System.out.println(game.getStartTime());
-        System.out.println(g2.getStartTime().equals(game.getStartTime()));
-        return;
+public class Main {
+    public static void main(String[] args) {
+    Game game =new Game();
+//    try {
+//     game.run();
+//    }catch(Exception ex) {
+//        System.out.println("Caught an Exception");
+//    }finally{
+//        System.out.println("Hit the finally");
+//    }
+    game.run();
+    long elapsedTicks = game.getEndTime().getTime() -
+            game.getStartTime().getTime();
+    double elapsedSeconds = elapsedTicks / 1000.0;
+    System.out.println("We were running for " + elapsedSeconds + "s.");
 
 
-//        boolean loop = true;
-//        while(loop) {
-//            System.out.print("> ");
-//            String input = scanner.nextLine().trim();
-//            //conditional statement
-//            if (input.equals("look")) {
-//                System.out.println("You look around.");
-//            }else if (input.equals("inventory")) {
-//                System.out.println("You are carrying nothing.");
-//            }else if (input.equalsIgnoreCase("dance")) {
-//                System.out.println("You dance around.");
-//            } else if (input.equalsIgnoreCase("jump")) {
-//                System.out.println("You jump in the air.");
-//            }else if (input.equalsIgnoreCase("exit")) {
-//                System.out.println("Goodbye.");
-//                loop =false;
-//            }else {
-//                System.out.println("Huh? I don't understand");
-//            }
-//        }
     }
 }
