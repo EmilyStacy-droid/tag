7572d5926e66080c1ae35c31e56dcceada8b32d3
Emily Stacy <Emily.Stacy@improving.com>
2019-09-27 12:24:53 -0500

    wrong aliases

--- src/main/java/org/improving/tag/commands/BaseEmoteCommand.java
+++ src/main/java/org/improving/tag/commands/BaseEmoteCommand.java
@@ -11,13 +11,15 @@ public abstract class BaseEmoteCommand extends BaseAliaseCommand {
     private InputOutput io;
     private String cmdText;
     private String cmdResponse;
+    private String aliases;
 //    private List<String> aliases = new ArrayList<>();
     //constructor to get value from two strings and io
     public BaseEmoteCommand (String cmdText, String cmdResponse, InputOutput io) {
+        super(...aliases);
         this.cmdText = cmdText;
         this.cmdResponse = cmdResponse;
           this.io = io;
-        super(cmdText);
+
 //        this.aliases.addA
 //        ll(Arrays.asList(aliases));
     }
